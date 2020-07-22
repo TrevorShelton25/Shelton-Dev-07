babf8865bc62d05fc9632348ad83feba4cc5435a
TrevorShelton25 <66640651+TrevorShelton25@users.noreply.github.com>
2020-07-22 01:11:54 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+function resetSign(event){
+    var signData = ['greeting', 'userSign', 'tiles', 'subTotal', 'shipping', 'grandTotal'];
+    
+    for (var i=0; i<signData.length; i++) {
+        setTextContentById(signData[i], '');
+        
+        
+    }
+    
+    event.preventDefault();
+    
+}
+
+var greeting = '[not initialized]' ; 
+var name = '[not initialized]';
+var message = '[not initialized]';
+var welcome;
+var sign = '[not initialized]';
+var tiles, subTotal, shipping, grandTotal;
+
+function calcSign() {
+    tiles = sign.length;
+    subTotal = tiles * 5;
+    shipping = 7;
+    grandTotal = subTotal + shipping;
+}
+
+function initiateVars() {
+    greeting = 'Howdy'; 
+    name = 'Trevor';
+    message = 'Plz check your order:';
+    sign = 'cats';
+    welcome = greeting + '! '+ name + ', ' + message;
+    calcSign();
+}
+
+function setTextContentById(getId, setMsg) {
+    var el = document.getElementById(getId);
+    el.textContent = setMsg;
+} 
+function setSign() {
+    setTextContentById('greeting', welcome);
+    setTextContentById('userSign', sign);
+    setTextContentById('tiles', tiles);
+    setTextContentById('subTotal', '$' + subTotal);
+    setTextContentById('shipping', '$' + shipping);
+    setTextContentById('grandTotal','$' + grandTotal);
+}
+
+initiateVars();
+setSign();
+//resetSign(event);
+
+
+var el = document.getElementById('resetButton');
+
+el.addEventListener('click', resetSign, false);
